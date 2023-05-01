aa69aef5d4e7c21b80b6e87e425f1458d7bc3bd5
Nigel Alford <ngalford@gmail.com>
2023-05-01 12:44:53 -0400

    setting async

--- src/webflow/setCenterFlow.ts
+++ src/webflow/setCenterFlow.ts
@@ -12,20 +12,15 @@ const { getCenter } = require('./getCenter');
 // 2. Run the script with the command `node setCenterFlow.js`.
 // 3. If the script fails, check the console output for error messages.
 
-const inputs = {
-  address: '3601 W. 145th St. Burnsville, MN',
-  centerName: 'Burnsville',
-  centerID: '001', // get this from the center slug
-  hubspotFormID: 'a0f0b0c0-d0e0-0000-0000-000000000000', // get this from the hubspot form
-};
+const centerCollectionId = '64492d7ee2522e3a782b51be';
 
 /**
  * Configuration object for axios requests.
- * @constant {Object}
- * @property {Object} headers - Request headers.
- * @property {string} headers.accept - Requested response format.
- * @property {string} headers.content-type - Request payload format.
- * @property {string} headers.authorization - Webflow API key.
+ * @constant
+ * @property headers - Request headers.
+ * @property headers.accept - Requested response format.
+ * @property headers.content-type - Request payload format.
+ * @property headers.authorization - Webflow API key.
  */
 const axiosConfig = {
   headers: {
@@ -37,65 +32,77 @@ const axiosConfig = {
 
 /**
  * Sends a POST request to the Webflow API to create an item in a collection.
- * @param {string} collectionId - ID of the target collection.
- * @param {Object} fields - Object containing field names and values.
- * @returns {Promise} Promise that resolves with the server response or rejects with an error.
+ * @param collectionId - ID of the target collection.
+ * @param fields - Object containing field names and values.
+ * @returns Promise that resolves with the server response or rejects with an error.
  */
-const setItem = (collectionId: String, fields: any) => {
-  console.info('setting with webflow api key: ', process.env.WEBFLOW_API_KEY)
-  return axios.post(
-    `https://api.webflow.com/collections/${collectionId}/items`,
-    {
-      fields,
-    },
-    axiosConfig,
-  );
+const setItem = async (collectionId: String, fields: any) => {
+  console.info('setting with webflow api key: ', process.env.WEBFLOW_API_KEY);
+  try {
+    return axios.post(
+      `https://api.webflow.com/collections/${collectionId}/items`,
+      {
+        fields,
+      },
+      axiosConfig,
+    );
+  } catch (error) {
+    console.error('error setting item: ', JSON.stringify(error.response.data));
+  }
 };
 
 /**
  * Sends a POST request to the Webflow API to create a center item.
- * @param {string} centerName - Name of the center to create.
- * @param {string} address - Address of the center to create.
- * @returns {Promise} Promise that resolves with the server response or rejects with an error.
+ * @param centerName - Name of the center to create.
+ * @param address - Address of the center to create.
+ * @returns Promise that resolves with the server response or rejects with an error.
  */
-const setCenter = (centerName: String, address: String) => {
-  const collectionId = '64492d7ee2522e3a782b51be';
+const setCenter = async (centerName: String, address: String) => {
+  const collectionId = centerCollectionId;
   const fields = getCenter(centerName, address);
   return setItem(collectionId, fields);
 };
 
 /**
  * Sends a POST request to the Webflow API to create an event item.
- * @param {string} centerName - Name of the center associated with the event.
- * @param {string} hubspotFormID - ID of the HubSpot form associated with the event.
- * @param {string|null} centerID - ID of the center associated with the event, or null if not available.
- * @returns {Promise} Promise that resolves with the server response or rejects with an error.
+ * @param centerName - Name of the center associated with the event.
+ * @param hubspotFormID - ID of the HubSpot form associated with the event.
+ * @param centerID - ID of the center associated with the event, or null if not available.
+ * @returns Promise that resolves with the server response or rejects with an error.
  */
-const setEvent = (
+const setEvent = async (
   centerName: String,
   hubspotFormID: String,
   centerID: String,
 ) => {
-  const collectionId = '64492d7ee2522e284a2b51cd';
-  const fields = getEvent(centerName, hubspotFormID, centerID || '');
-  return setItem(collectionId, fields);
+  try {
+    const collectionId = '64492d7ee2522e284a2b51cd';
+    const fields = getEvent(centerName, hubspotFormID, centerID || '');
+    return setItem(collectionId, fields);
+  } catch (event) {
+    console.error('error setting event: ', JSON.stringify(event.response.data));
+  }
 };
 
 /**
  * Sends a POST request to the Webflow API to create a center carousel item.
- * @param {string} centerName - Name of the center associated with the carousel.
- * @returns {Promise} Promise that resolves with the server response or rejects with an error.
+ * @param  centerName - Name of the center associated with the carousel.
+ * @returns Promise that resolves with the server response or rejects with an error.
  */
-const setCenterCarousel = (centerName: String) => {
-  const collectionId = '64492d7ee2522e32452b51c3';
-  const fields = getCarousel(centerName);
-  return setItem(collectionId, fields);
+const setCenterCarousel = async (centerName: String) => {
+  try {
+    const collectionId = '64492d7ee2522e32452b51c3';
+    const fields = getCarousel(centerName);
+    return setItem(collectionId, fields);
+  } catch (event) {
+    console.error('error setting event: ', JSON.stringify(event.response.data));
+  }
 };
 
 /**
  * Sends multiple POST requests to the Webflow API to create program items for a center.
- * @param {string} centerName - Name of the center associated with the programs.
- * @returns {Promise} Promise that resolves with an array of server responses or rejects with an error.
+ * @param  centerName - Name of the center associated with the programs.
+ * @returns Promise that resolves with an array of server responses or rejects with an error.
  */
 const setCenterPrograms = async (centerName: String) => {
   const collectionId = '64492d7ee2522e4b272b51c9';
