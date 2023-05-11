2d3b4214f7fa186e6c90ff2f3d24953cd851bfc5
Nigel Alford <ngalford@gmail.com>
2023-05-10 20:21:10 -0400

    added template and holder data

---
+++ src/templates/allCollections.ts
@@ -0,0 +1,164 @@
+const Collections = [
+    {
+        "_id": "64492d7ee2522e104f2b51c2",
+        "lastUpdated": "2023-04-26T13:56:16.370Z",
+        "createdOn": "2021-04-02T18:37:23.290Z",
+        "name": "Journal Posts",
+        "slug": "journal-post",
+        "singularName": "Journal Post"
+    },
+    {
+        "_id": "64492d7ee2522e21952b51c7",
+        "lastUpdated": "2023-04-26T13:56:16.403Z",
+        "createdOn": "2021-07-22T22:39:30.960Z",
+        "name": "Policies",
+        "slug": "policies",
+        "singularName": "Policy"
+    },
+    {
+        "_id": "64492d7ee2522e23782b51c6",
+        "lastUpdated": "2023-04-26T13:56:16.398Z",
+        "createdOn": "2021-04-02T20:14:43.316Z",
+        "name": "SKUs",
+        "slug": "sku",
+        "singularName": "SKU"
+    },
+    {
+        "_id": "64492d7ee2522e242c2b51c1",
+        "lastUpdated": "2023-04-26T13:56:16.365Z",
+        "createdOn": "2022-06-04T05:23:11.675Z",
+        "name": "Sub Programs",
+        "slug": "center-program-subprogram",
+        "singularName": "Sub Program"
+    },
+    {
+        "_id": "64492d7ee2522e284a2b51cd",
+        "lastUpdated": "2023-04-27T03:33:50.166Z",
+        "createdOn": "2022-03-16T17:34:50.289Z",
+        "name": "Events",
+        "slug": "events",
+        "singularName": "Event"
+    },
+    {
+        "_id": "64492d7ee2522e32452b51c3",
+        "lastUpdated": "2023-04-26T13:56:16.382Z",
+        "createdOn": "2021-11-10T21:08:12.227Z",
+        "name": "Center Carousels",
+        "slug": "center-carousels",
+        "singularName": "Center Carousel"
+    },
+    {
+        "_id": "64492d7ee2522e3a782b51be",
+        "lastUpdated": "2023-04-27T14:42:55.924Z",
+        "createdOn": "2021-04-07T14:58:38.427Z",
+        "name": "Centers",
+        "slug": "centers",
+        "singularName": "Center"
+    },
+    {
+        "_id": "64492d7ee2522e49ab2b51d0",
+        "lastUpdated": "2023-04-26T13:56:16.460Z",
+        "createdOn": "2021-04-15T16:11:42.772Z",
+        "name": "Countries",
+        "slug": "countries",
+        "singularName": "Country"
+    },
+    {
+        "_id": "64492d7ee2522e4b272b51c9",
+        "lastUpdated": "2023-05-09T21:09:54.098Z",
+        "createdOn": "2021-06-16T16:12:21.519Z",
+        "name": "Center Programs",
+        "slug": "center-programs",
+        "singularName": "Center Program"
+    },
+    {
+        "_id": "64492d7ee2522e58f82b51c8",
+        "lastUpdated": "2023-04-26T13:56:16.412Z",
+        "createdOn": "2021-04-14T17:06:42.736Z",
+        "name": "Trainers",
+        "slug": "trainers",
+        "singularName": "Trainer"
+    },
+    {
+        "_id": "64492d7ee2522e5eca2b51cb",
+        "lastUpdated": "2023-04-26T13:56:16.432Z",
+        "createdOn": "2022-02-01T18:54:28.809Z",
+        "name": "Group Training Programs",
+        "slug": "group-training",
+        "singularName": "Group Training Program"
+    },
+    {
+        "_id": "64492d7ee2522e7cc62b51c5",
+        "lastUpdated": "2023-04-26T13:56:16.393Z",
+        "createdOn": "2022-04-04T23:16:13.292Z",
+        "name": "Partnerships",
+        "slug": "partnerships",
+        "singularName": "Partnership"
+    },
+    {
+        "_id": "64492d7ee2522e99a42b51ca",
+        "lastUpdated": "2023-04-26T13:56:16.423Z",
+        "createdOn": "2022-02-14T22:55:19.477Z",
+        "name": "Kids in Sports Programs",
+        "slug": "kids-in-sports",
+        "singularName": "Kids in Sports Program"
+    },
+    {
+        "_id": "64492d7ee2522ea2db2b51cc",
+        "lastUpdated": "2023-04-26T13:56:16.439Z",
+        "createdOn": "2021-09-14T21:52:42.331Z",
+        "name": "Center Features",
+        "slug": "center-features",
+        "singularName": "Center Feature"
+    },
+    {
+        "_id": "64492d7ee2522ea8de2b51cf",
+        "lastUpdated": "2023-04-26T13:56:16.455Z",
+        "createdOn": "2021-04-07T14:55:45.435Z",
+        "name": "States",
+        "slug": "states",
+        "singularName": "State"
+    },
+    {
+        "_id": "64492d7ee2522eb3232b51c0",
+        "lastUpdated": "2023-04-26T13:56:16.348Z",
+        "createdOn": "2022-04-05T20:20:33.435Z",
+        "name": "Closing Facilities",
+        "slug": "closing",
+        "singularName": "Closing Facility"
+    },
+    {
+        "_id": "64492d7ee2522ebe3b2b51bf",
+        "lastUpdated": "2023-04-26T13:56:16.341Z",
+        "createdOn": "2021-04-02T20:14:43.314Z",
+        "name": "Categories",
+        "slug": "category",
+        "singularName": "Category"
+    },
+    {
+        "_id": "64492d7ee2522ec9322b51d1",
+        "lastUpdated": "2023-04-26T13:56:16.464Z",
+        "createdOn": "2021-12-31T18:47:52.076Z",
+        "name": "Strikers Programs",
+        "slug": "strikers",
+        "singularName": "Strikers Program"
+    },
+    {
+        "_id": "64492d7ee2522eda282b51ce",
+        "lastUpdated": "2023-04-26T13:56:16.449Z",
+        "createdOn": "2021-04-02T20:14:43.311Z",
+        "name": "Vacation Invoices",
+        "slug": "product",
+        "singularName": "Vacation Invoice"
+    },
+    {
+        "_id": "64492d7ee2522ee4632b51c4",
+        "lastUpdated": "2023-04-26T13:56:16.387Z",
+        "createdOn": "2021-09-29T21:20:19.068Z",
+        "name": "FAQs",
+        "slug": "faq",
+        "singularName": "FAQ"
+    }
+];
+
+export default Collections;
---
+++ src/webflow/getEvent.ts
@@ -0,0 +1,29 @@
+import { Center } from "../types/webflow";
+
+/**
+ * Returns an object containing the fields for creating a new event item.
+ * @param {string} center.name - Name of the center associated with the event.
+ * @param {string} center.hubspotFormID - ID of the HubSpot form associated with the event.
+ * @param {string} centerID - ID of the center associated with the event.
+ * @returns {Object} Object containing the fields for creating a new event item.
+ */
+const getEvent = (center: Center, centerID: string) => ({
+    "_archived": false,
+    "_draft": false,
+    "name": `${center.name} Free Kickoff`,
+    "hubspot-form-id": center.hubspotFormID,
+    "blurb": "Try TOCA For FREE!",
+    "description": `<h4 id=\"\">Next Generation Soccer Training</h4><p id=\"\">TOCA offers technology-enhanced soccer training designed to produce rapid skill improvement and develop well-rounded players. The key is TOCA’s proprietary technology, including the Touch Trainer, Smart Targets, Studio Screens, and personalized data tracking, that take your game to the next level.&nbsp;</p><p id=\"\">To learn more about our training program, visit <a href=\"/center-programs/${center.name}-1abouttocafootball\" id=\"\">About TOCA Football</a>.</p><h4 id=\"\">Try TOCA For FREE!</h4><p id=\"\">All new guests get a free player assessment!</p><ul id=\"\"><li id=\"\">Try our technology including the Touch Trainer and Smart Targets</li><li id=\"\">Discuss your training goals with our trainers</li><li id=\"\">Find time that works for you! We have flexible schedules and look forward to seeing you!</li></ul><h4 id=\"\">How To Sign Up For a FREE Kickoff</h4><p id=\"\">Fill out the form on this page and select <strong id=\"\">Free Kickoff</strong> under program. Our center will contact you shortly to book your free kickoff session!</p><p id=\"\">‍</p>`,
+    "slug": `${center.name}-free-kickoff`,
+    "hero-slider-image-first": {
+      "fileId": "64492d7ee2522e648d2b564a",
+      "url": "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/63263b3bc587850566dd1b0b_210608_ha_toca_training_028_LowRes.jpg",
+      "alt": null
+    },
+    "center": centerID,
+    "mytoca-2": false,
+    "call-only": false,
+  });
+  
+export default getEvent;
+  
