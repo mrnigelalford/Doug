a5c25b7cf0550da802bcc15014329aee52af9bbb
Nigel Alford <ngalford@gmail.com>
2023-05-23 10:35:36 -0400

    updated images

--- src/localDev.ts
+++ src/localDev.ts
@@ -11,21 +11,3 @@ setEvent(
   },
   '644aef5d10974d337ddb7de5',
 );
-
-// const sample = {
-//     "_archived": false,
-//     "_draft": false,
-//     "mytoca-2": false,
-//     "call-only": false,
-//     "name": "Loganville Free Kickoff",
-//     "hubspot-form-id": "a0f0b0c0-d0e0-0000-0000-000000000000",
-//     "blurb": "Try TOCA For FREE!",
-//     "description": "<h4 id=\"\">Next Generation Soccer Training</h4><p id=\"\">TOCA offers technology-enhanced soccer training designed to produce rapid skill improvement and develop well-rounded players. The key is TOCA’s proprietary technology, including the Touch Trainer, Smart Targets, Studio Screens, and personalized data tracking, that take your game to the next level.&nbsp;</p><p id=\"\">To learn more about our training program, visit <a href=\"/center-programs/Loganville-1abouttocafootball\" id=\"\">About TOCA Football</a>.</p><h4 id=\"\">Try TOCA For FREE!</h4><p id=\"\">All new guests get a free player assessment!</p><ul id=\"\"><li id=\"\">Try our technology including the Touch Trainer and Smart Targets</li><li id=\"\">Discuss your training goals with our trainers</li><li id=\"\">Find time that works for you! We have flexible schedules and look forward to seeing you!</li></ul><h4 id=\"\">How To Sign Up For a FREE Kickoff</h4><p id=\"\">Fill out the form on this page and select <strong id=\"\">Free Kickoff</strong> under program. Our center will contact you shortly to book your free kickoff session!</p><p id=\"\">‍</p>",
-//     "slug": "Loganville-free-kickoff",
-//     "hero-slider-image-first": {
-//         "fileId": "6451a7ebe505d96529e2d876",
-//         "url": "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/6451a7ebe505d96529e2d876_644aef63132d320b5cb0fea0_63263b3bc587850566dd1b0b_210608_ha_toca_training_028_LowRes%20(1).jpeg",
-//         "alt": null
-//     },
-//     "center": "644aef5d10974d337ddb7de5",
-// },
--- src/webflow/getCarousel.ts
+++ src/webflow/getCarousel.ts
@@ -1,3 +1,5 @@
+import { capitalizeFirstLetter } from "../templates/nashville";
+
 /**
 
 Returns an object with the fields needed to create a Webflow carousel item for a given center.
@@ -6,39 +8,47 @@ Returns an object with the fields needed to create a Webflow carousel item for a
 */
 export const getCarousel = (centerName: string) => ({
   _archived: false,
-  _draft: false,
-  "cta-link-3-2": `https://www.tocafootball.com/center-programs/${centerName}-2soccer-04grouptraining`,
-  "cta-link-1": `https://www.tocafootball.com/center-programs/${centerName}-2soccer-01freekickoff`,
-  "cta-link-2": `https://www.tocafootball.com/center-programs/${centerName}-2soccer-02individualtraining`,
-  "sub-text-3": `Join Small Group Training`,
+  _draft: true,
+  "cta-link-3-2":
+    `https://www.tocafootball.com/center-programs/${center.name.toLowerCase()}-2soccer-06kidsclasses`,
+  "cta-link-1":
+    `https://www.tocafootball.com/center-programs/${center.name.toLowerCase()}-2soccer-01freekickoff`,
+  "cta-link-2":
+    `https://www.tocafootball.com/center-programs/${center.name.toLowerCase()}-2soccer-02individualtraining`,
+  "sub-text-3": "Introduce Your Child to Soccer",
   "main-text-1": "Free Kickoff",
   "main-text-2": "Individual Training",
-  "main-text-3": "Group Training",
-  name: `${centerName} Carousel`,
+  "main-text-3": "Kids' Classes",
+  name: `${capitalizeFirstLetter(centerName)} Carousel`,
   "cta-text-1": "Book Now",
   "cta-text-2": "Learn More",
   "cta-text-3": "Learn More",
   "sub-text-1": "Try A FREE Lesson!",
   "sub-text-2": "Train with TOCA Technology!",
-  slug: `${centerName}-carousel`,
-  "image-3": {
-    fileId: "64492d7ee2522e6fcb2b56d8",
-    url: "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/634d6f5e9dbdd2448475af89_group-training.jpg",
+  "image-1": {
+    fileId: "64492d7ee2522ecdd12b56e1",
+    url: `https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/634ee33f05955eedde7f468a_individual-training.jpg`,
     alt: null,
   },
   "image-2": {
     fileId: "64492d7ee2522ea09e2b56c5",
-    url: "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/63462cd0c2423e5e6cd7751a_634628a145d4a188a2b6bab8_carousel-individual.jpeg",
+    url: `https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/63462cd0c2423e5e6cd7751a_634628a145d4a188a2b6bab8_carousel-individual.jpeg`,
     alt: null,
   },
-  "image-1": {
-    fileId: "64492d7ee2522ecdd12b56e1",
-    url: "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/634ee33f05955eedde7f468a_individual-training.jpg",
+  "image-3": {
+    fileId: "64629765a1057dbf5f16e7e1",
+    url: `https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/64629765a1057dbf5f16e7e1_kids%20playing%20soccer.jpg`,
     alt: null,
   },
+  slug: `${centerName.toLowerCase()}-carousel`,
+  "cta-link-4-2":
+    `https://www.tocafootball.com/center-programs/${centerName.toLowerCase()}-3birthdayparties`,
+  "cta-text-4-2": "Learn More",
   "image-4-2": {
-    fileId: "64492d7ee2522eae4b2b56f5",
-    url: "https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/6419cc800a9ea38b3043d6cf_junior-field-4-e1541817254150.jpg",
+    fileId: "64492d7ee2522ea55d2b56c3",
+    url: `https://uploads-ssl.webflow.com/60c7be61132e3ad0b40a333d/634629059cc86594d2f32804_carousel-bday.jpg`,
     alt: null,
   },
+  "main-text-4-2": "Birthday Parties",
+  "sub-text-4-2": "Celebrate Your Birthday with TOCA!",
 });
